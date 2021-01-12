5e32e9ad61ed221713fc45b8e2b4588dd5df1ccd
kokok22 <kyjin2881@ajou.ac.kr>
2021-01-12 18:23:31 +0900

    210112

--- 2. Array/ProductOfArrayExceptSelf.py
+++ 2. Array/ProductOfArrayExceptSelf.py
@@ -4,8 +4,22 @@
 
 def prodcutExceptSelf(nums):
     # two point를 활용하여 문제를 풀어본다.
-    for idx, num in enumerate(nums):
-        pass
+    out = []
+    p=1
+
+    # 왼쪽 곱셈
+    for i in range(0, len(nums)):
+        out.append(p)
+        p = p * nums[i]
+
+    p = 1
+
+    # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+    for i in range(len(nums) - 1, 0 -1, -1):
+        out[i] = out[i] * p
+        p = p * nums[i]
+
+    return out
 
 
 if __name__ == "__main__":
