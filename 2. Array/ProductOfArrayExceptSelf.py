7d0b552e1606596cf45c53062f5a0fab93f13829
kokok22 <kyjin2881@ajou.ac.kr>
2021-01-12 10:02:22 +0900

    210112

---
+++ 2. Array/ProductOfArrayExceptSelf.py
@@ -0,0 +1,13 @@
+## 배열을 입력받아 output[i]가 자신을 제외한 나머지 모든 요소의 곱셈 결과가 되도록 출력하라
+## 단, 나눗셈을 하지 않고 O(n)에 풀어야 한다.
+
+
+def prodcutExceptSelf(nums):
+    # two point를 활용하여 문제를 풀어본다.
+    for idx, num in enumerate(nums):
+        pass
+
+
+if __name__ == "__main__":
+    nums = [1,2,3,4]
+    print(prodcutExceptSelf(nums))
