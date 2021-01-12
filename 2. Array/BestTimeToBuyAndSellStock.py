68e29890cb94f6feb693742f373ce605d84eb9f7
kokok22 <kyjin2881@ajou.ac.kr>
2021-01-12 20:23:58 +0900

    210112

---
+++ 2. Array/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,20 @@
+## 한 번의 거래로 낼 수 있는 최대 이익을 산출하라
+
+
+def maxProfit(prices):
+    profit = 0
+    min_price = 10000
+
+    # 앞으로 가면서 비교를 하는 것이다.
+    for price in prices:
+        # 비교할 때 min, max로 하자
+        min_price = min(price, min_price)
+        profit = max(price - min_price, profit)
+
+    return profit
+
+
+
+if __name__ == "__main__":
+    prices = [7,1,5,3,6,4]
+    print(maxProfit(prices))
--- 2. Array/ProductOfArrayExceptSelf.py
+++ 2. Array/ProductOfArrayExceptSelf.py
@@ -5,19 +5,19 @@
 def prodcutExceptSelf(nums):
     # two point를 활용하여 문제를 풀어본다.
     out = []
-    p=1
+    p = 1
 
     # 왼쪽 곱셈
-    for i in range(0, len(nums)):
+    for i in range(len(nums)):
         out.append(p)
-        p = p * nums[i]
+        p *= nums[i]
 
     p = 1
 
     # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
-    for i in range(len(nums) - 1, 0 -1, -1):
-        out[i] = out[i] * p
-        p = p * nums[i]
+    for i in range(len(nums)-1, -1, -1):
+        out[i] *= p
+        p *= nums[i]
 
     return out
 
