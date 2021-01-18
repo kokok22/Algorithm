9f816c94f2f7420c49a1ffb9540271092f1766d7
Kwon Yongjin <kyjin2881@ajou.ac.kr>
2021-01-18 17:12:57 +0900

    210118

---
+++ Programmers/Level_1/Divisor2.py
@@ -0,0 +1,13 @@
+import math
+
+def solution(n):
+    result = set()
+
+    for i in range(1, int(math.sqrt(n))+1):
+        if n % i == 0:
+            result.add(i)
+            result.add(n // i)
+
+    return sum(result)
+
+print(solution(10))
