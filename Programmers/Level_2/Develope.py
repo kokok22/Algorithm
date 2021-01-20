76b47131728ebc89d4e56d0211d7781e1cb07b15
Kwon Yongjin <kyjin2881@ajou.ac.kr>
2021-01-20 23:57:36 +0900

    210120

--- Programmers/Level_2/Develope.py
+++ Programmers/Level_2/Develope.py
@@ -15,4 +15,32 @@ def solution(progresses, speeds):
     answer.append(count)
     return answer
 
+
+# def solution(progresses, speeds):
+#     answer = []
+#     queue = []
+#     pivot = 0
+#     size = len(speeds)
+#
+#     while pivot != size:
+#         for idx, speed in enumerate(speeds):
+#             if progresses[idx] < 100:
+#                 progresses[idx] += speed
+#
+#             elif progresses[idx] >= 100:
+#                 queue.append(idx)
+#                 progresses[idx] = 0
+#                 speeds[idx] = 0
+#
+#         count = 0
+#         while pivot in queue:
+#             del queue[queue.index(pivot)]
+#             count += 1
+#             pivot += 1
+#
+#         if count:
+#             answer.append(count)
+#
+#     return answer
+
 print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
