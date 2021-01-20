84bbe028c78191b5a95a192b32cdc4733328fdb9
Kwon Yongjin <kyjin2881@ajou.ac.kr>
2021-01-21 01:08:25 +0900

    210121

---
+++ Programmers/Level_2/Skill Tree.py
@@ -0,0 +1,41 @@
+def solution(skill, skill_trees):
+    answer = 0
+
+    for skills in skill_trees:
+        skill_list = list(skill)
+
+        for s in skills:
+            if s in skill:
+                if s != skill_list.pop(0):
+                    break
+        else:
+            answer += 1
+
+    return answer
+
+# def solution(skill, skill_trees):
+#     count = 0
+#
+#     for skill_tree in skill_trees:
+#         temp = ''
+#         for char in skill_tree:
+#             if char in skill:
+#                 temp += char
+#
+#         idx = 0
+#         flag = True
+#
+#         while idx < len(temp):
+#             if temp[idx] != skill[idx]:
+#                 flag = False
+#                 break
+#             else:
+#                 idx += 1
+#
+#         if flag:
+#             count += 1
+#
+#     return count
+
+
+print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
