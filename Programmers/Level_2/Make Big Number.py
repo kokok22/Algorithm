fb3e06d3ef332f507962464afa9849993717787a
Kwon Yongjin <kyjin2881@ajou.ac.kr>
2021-01-23 03:03:57 +0900

    210123

---
+++ Programmers/Level_2/Make Big Number.py
@@ -0,0 +1,22 @@
+def solution(number, k):
+    answer = []
+    size = len(number) - k
+    idx = 0
+
+    for i in range(size):
+        max_num = '0'
+        idx = 0
+        for j in range(k + 1):
+            if max_num < number[j]:
+                max_num = number[j]
+                idx = j
+                if max_num == '9':
+                    break
+
+        answer.append(max_num)
+        number = number[idx+1:]
+        k -= idx
+
+    return ''.join(answer)
+
+print(solution("4177252841", 4))
